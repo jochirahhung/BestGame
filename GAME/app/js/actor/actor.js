434d4289df2bce5a959b32dfd9c6224994b5b1c4
zpeterson22 <zachrp2296@yahoo.com>
2018-07-25 08:50:35 -0600

    Added Actor file
    Used for player or enemy

---
+++ GAME/app/js/actor/actor.js
@@ -0,0 +1,10 @@
+function Actor(nameString, x, y, r) {
+    this.name = nameString;
+    this.pos = {x: x, y: y};
+
+    this.boundsRadius = r;
+}
+
+Actor.prototype.update = function(dt) {
+    
+}
