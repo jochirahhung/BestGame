a1dc517ee58fc1a7c5e7772971c1b8995966e79e
zpeterson22 <zachrp2296@yahoo.com>
2018-07-25 21:48:17 -0600

    Player Movement added
    added base movement for play

--- GAME/app/app.js
+++ GAME/app/app.js
@@ -13,17 +13,31 @@ var app = {
         x: 0,
         y: 0
     },
-    playerObject: null,
+    playerChar: null,
+    debugLine: null,
     setupCanvas: function() {
         var canvas = document.getElementById("game");
-        canvas.width = settings.SCREEN_WIDTH;
-        canvas.height = settings.SCREEN_HEIGHT;
+        canvas.width = app.SCREEN_WIDTH;
+        canvas.height = app.SCREEN_HEIGHT;
         this.stage = new createjs.Stage(canvas);
     },
     beginLoad: function() {
-        manifest: [
+        manifest = [
+            // {
+            //     src: "js/actor/actor.js"
+            // }
+        ];
+        this.assets = new createjs.LoadQueue(true);
 
-        ]
+        this.assets.on("progress", function (event) { 
+            // console.log(((event.loaded / event.total) * 100) + "%"); 
+        });
+
+        this.assets.on("complete", function (event) {
+            app.init();
+        });
+
+        this.assets.loadManifest(manifest);
     },
     init: function() {
         this.setupCanvas();
@@ -36,9 +50,23 @@ var app = {
             console.log("AAAAAAAAAAAAAAAAAAAAA: ( " + app.mousePos.x + ", " + app.mousePos.y + ")");
         });
 
+        this.playerChar = new createjs.Shape();
+        this.playerChar.graphics.beginFill('#fffff').drawRect(0, 0, 25, 25);
+        // this.playerChar.getBounds(this.playerChar.setBounds(12.5,12.5,0,0));
+        this.playerChar.x = this.SCREEN_WIDTH/2;
+        this.playerChar.y = this.SCREEN_HEIGHT/2;
+        this.stage.addChild(this.playerChar);  
+
+        this.debugLine = new createjs.Shape();
+        this.debugLine.graphics.beginStroke('00f').moveTo(this.playerChar.x, this.playerChar.y).lineTo(app.mousePos.x, app.mousePos.y);
+        this.stage.addChild(this.debugLine);
+
         this.stage.on("stagemousedown", function(event) {
             app.handleMouseDown(event);
         });
+
+        document.onkeydown = this.handleKeyDown;
+        document.onkeyup = this.handleKeyUp;
 
         createjs.Ticker.addEventListener("tick", this.update);
         createjs.Ticker.framerate = this.FPS;   
@@ -47,6 +75,35 @@ var app = {
         app.stage.update(event);
         var dt = event.delta / 1000;
         app.elapsedTime += dt;
+
+        app.debugLine.graphics.clear();
+        app.debugLine.graphics.beginStroke('00f').moveTo(app.playerChar.x, app.playerChar.y).lineTo(app.mousePos.x, app.mousePos.y);
+        var ROT_SPEED = 100;
+        var MOVE_SPEED = 100;
+        
+        var rotation = app.playerChar.rotation / 360 * 2 * Math.PI;
+
+        if(app.keyboard.w.isPressed)
+        {
+            app.playerChar.y -= MOVE_SPEED * dt;
+        }
+        
+        if(app.keyboard.s.isPressed)
+        {
+            app.playerChar.y += MOVE_SPEED * dt;
+        }
+        
+        if(app.keyboard.d.isPressed) {
+            app.playerChar.x += MOVE_SPEED * dt;
+        }
+        
+        if(app.keyboard.a.isPressed) {
+            app.playerChar.x -= MOVE_SPEED * dt;
+        }
+
+        var angleRad = Math.atan2(app.mousePos.y - app.playerChar.y, app.mousePos.x - app.playerChar.x);
+        var angleDeg = angleRad * 180 / Math.PI;
+        app.playerChar.rotation = angleDeg;
     },
     handleKeyDown: function(event)
     {
@@ -75,3 +132,5 @@ var app = {
         }
     }
 }
+
+app.init();
--- GAME/app/js/actor/actor.js
+++ GAME/app/js/actor/actor.js
@@ -9,3 +9,18 @@ Actor.prototype.update = function(dt) {
 
 }
 
+function Player() {
+    Actor.call(this, nameString, x, y, r);
+
+    this.playerChar = new createjs.Shape();
+    this.playerChar.graphics.beginFill('#000000').drawRect(0,0, app.SCREEN_WIDTH, app.SCREEN_HEIGHT);
+
+    this.update = function(dt) {
+        console.log("We in this loop");
+        Actor.prototype.update.call(this, dt);
+    }
+
+    app.stage.addChild(playerChar);
+}
+Player.prototype = Object.create(Actor.prototype);
+Player.prototype.constructor = Player;
